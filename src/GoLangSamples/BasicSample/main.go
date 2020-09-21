fb0ce08850dd28663b945d16acbd03ca393bfc42
AbhiOnGithub <innovative.abhishek@hotmail.com>
2020-09-22 03:11:14 +0530

    added printing Container Id / Hostname

--- src/GoLangSamples/BasicSample/main.go
+++ src/GoLangSamples/BasicSample/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -12,11 +13,20 @@ type incomingEvent struct {
 	Data interface{} `json:"data"`
 }
 
+func hostName() string {
+	name, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	return name
+}
+
 func main() {
 
 	http.HandleFunc("/",
 		func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Welcome to first GoLang Web Program running using DAPR! "+time.Now().Format("2006-01-02 15:04:05")) //YYYY-MM-DD hh:mm:ss
+			fmt.Fprintf(w, "\n HostName "+hostName())
 		})
 
 	http.HandleFunc("/greeting",
@@ -26,6 +36,7 @@ func main() {
 			decoder.Decode(&event)
 			fmt.Println(event.Data)
 			fmt.Fprintf(w, "Welcome, Greetings of the day from DAPR !! "+time.Now().Format("2006-01-02 15:04:05"))
+			fmt.Fprintf(w, "\n HostName "+hostName())
 		})
 
 	http.HandleFunc("/greetme",
@@ -44,7 +55,8 @@ func main() {
 			// we only want the single item.
 			name := keys[0]
 
-			fmt.Fprintf(w, "Welcome "+string(name)+", Greetings of the day from DAPR !! "+time.Now().Format("2006-01-02 15:04:05"))
+			fmt.Fprintf(w, " Welcome "+string(name)+", Greetings of the day from DAPR !! "+time.Now().Format("2006-01-02 15:04:05"))
+			fmt.Fprintf(w, "\n HostName "+hostName())
 		})
 
 	log.Fatal(http.ListenAndServe(":8088", nil))
